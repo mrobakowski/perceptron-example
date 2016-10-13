e01a9c778c214d8ec9611de4ad4cdc8bfddda0b8
Mikołaj Robakowski <mikolaj.rob@gmail.com>
2016-10-13 18:46:57 +0200

    start learning

--- src/main/kotlin/com/github/mrobakowski/perceptron/Perceptron.kt
+++ src/main/kotlin/com/github/mrobakowski/perceptron/Perceptron.kt
@@ -1,13 +1,43 @@
 package com.github.mrobakowski.perceptron
-import golem.mapMat
-import golem.mat
+
+import golem.*
 import golem.matrix.Matrix
-import golem.randn
 
 class Perceptron(
-        val numInputs: Int,
-        val activationFunction: (Matrix<Double>) -> Matrix<Double> = { it.mapMat { (it >= 0).toDouble() } }
+        numInputs: Int,
+        val activationFunction: (Matrix<Double>) -> Matrix<Double> = { it.mapMat { (it >= 0.5).toDouble() } }
 ) {
-    var weights = randn(numInputs + 1 /*bias*/) / 2
+    var weights = randn(numInputs + 1 /*bias*/)
     fun response(x: Matrix<Double>) = activationFunction(weights * x.extendWithColumns(mat[1]).T)
+
+    fun learn(examples: List<DataPoint>, trainRate: Double = 0.1, maxEpochs: Int = 5) {
+        val shuffled = examples.shuffle()
+
+        for (epoch in 0..maxEpochs) {
+            var totalError = 0.0
+            for ((inputs, desiredOutput) in shuffled) {
+                val output = response(inputs)
+                val error = desiredOutput - output
+                totalError += abs(error).sum()
+                weights += trainRate * error * inputs.extendWithColumns(mat[1])
+            }
+
+            println("Epoch #$epoch err=$totalError")
+
+            if (totalError == 0.0) break
+        }
+    }
+}
+
+data class DataPoint(val input: Matrix<Double>, val output: Matrix<Double>) {
+    constructor(combined: Matrix<Double>) : this(
+            combined[
+                    0..(combined.numRows() - 1),
+                    0..(combined.numCols() - 2)
+                    ],
+            combined[
+                    0..(combined.numRows() - 1),
+                    combined.numCols() - 1
+                    ]
+    )
 }
--- src/main/kotlin/com/github/mrobakowski/perceptron/matrixExtensions.kt
+++ src/main/kotlin/com/github/mrobakowski/perceptron/matrixExtensions.kt
@@ -3,6 +3,8 @@ package com.github.mrobakowski.perceptron
 import golem.mapMatIndexed
 import golem.mapRows
 import golem.matrix.Matrix
+import java.util.*
+
 
 fun Boolean.toInt() = if (this) 1 else 0
 fun Boolean.toDouble() = if (this) 1.0 else 0.0
@@ -20,3 +22,16 @@ infix fun Matrix<Double>.eq(other: Matrix<Double>) =
         this.mapMatIndexed { i, j, v -> if (v == other[i, j]) 0.0 else 1.0 }.sum() == 0.0
 
 class InvalidDimensionsException(expected: Int, got: Int) : Exception("Invalid dimensions - expected: $expected, got: $got")
+
+
+fun <T> List<T>.shuffle(): List<T> {
+    val items = this.toMutableList()
+    val rg: Random = Random()
+    for (i in 0..items.size - 1) {
+        val randomPosition = rg.nextInt(items.size)
+        val tmp: T = items[i]
+        items[i] = items[randomPosition]
+        items[randomPosition] = tmp
+    }
+    return items
+}
